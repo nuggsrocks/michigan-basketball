997d851ba9422b1e55034dc08f41339fe941ea06
mattfisher <nuggsrocks@yahoo.com>
2020-05-22 18:43:30 -0400

    fixed hot module reloading

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
+if(typeof(module.hot) !== 'undefined') { // eslint-disable-line no-undef
+    module.hot.accept() // eslint-disable-line no-undef
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
