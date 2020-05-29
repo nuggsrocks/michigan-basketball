98db994ea6deb7d13470ce1de6871f29bc84d53d
mattfisher <nuggsrocks@yahoo.com>
2020-05-29 00:58:13 -0400

    eslint react version detect

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
-    plugins: ["react"],
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    parser: "babel-eslint",
-    env: {
-        node: true,
-        browser: true
-    },
-    rules: {
-        "react/prop-types": [0, {skipUndeclared: true}]
-    }
+	plugins: ["react"],
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended"
+	],
+	parser: "babel-eslint",
+	settings: {
+		react: {
+			version: "detect"
+		}
+	},
+	env: {
+		node: true,
+		browser: true
+	},
+	rules: {
+		"react/prop-types": [0, {skipUndeclared: true}]
+	}
 }
