4c69f9d7c73804c6bbe6dc3e9bcfa6dc38bbb6be
mattfisher <nuggsrocks@yahoo.com>
2020-05-29 00:54:17 -0400

    some formatting changes

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     output: {
         path: __dirname + '/dist',
-        publicPath: '/michigan-basketball',
+        publicPath: '/',
         filename: '[name].js'
     },
     target: 'web',
