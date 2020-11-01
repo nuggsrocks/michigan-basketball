8e9df667d974ac2926cefe027bba3c6d47aa1a8b
mattfisher <nuggsrocks@yahoo.com>
2020-11-01 17:09:58 -0500

    split code

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './js/index.js',
+	output: {
+		path: __dirname + '/public',
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.html$/,
+				loader: 'html-loader'
+			},
+			
+			{
+				test: /\.(jpe?g|png|gif|wav|mp3)/,
+				loader: 'file-loader'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'index.html',
+			filename: 'index.html',
+			meta: {
+				charset: 'utf-8',
+				viewport: 'width=device-width,initial-scale=1.0'
+			}
+		})
+	]
+}
