38f2a9cc3372d41b244e0a6e9f1da696572b286e
mattfisher <nuggsrocks@yahoo.com>
2020-11-01 18:08:39 -0500

    added percentage calculations for stat leaders

--- js/components/schedule.js
+++ js/components/schedule.js
@@ -45,6 +45,12 @@ const StatLeaders = (props) => {
 	const displayStatLeaders = () => {
 		let statCategories = stats.length > 0 ? Object.keys(stats[0].data) : [];
 
+		statCategories.splice(statCategories.indexOf('FGM'), 2, 'FG%');
+
+		statCategories.splice(statCategories.indexOf('FTM'), 2, 'FT%');
+
+		statCategories.splice(statCategories.indexOf('3PM'), 2, '3P%');
+
 		let statLeaders = {};
 
 		statCategories.forEach((statName) => {
@@ -54,6 +60,10 @@ const StatLeaders = (props) => {
 
 				if (statName === 'GP') {
 					result =  b.data[statName] - a.data[statName];
+				} else if (statName.search(/%/) !== -1) {
+					let statMade = statName.replace('%', 'M');
+					let statAtt = statName.replace('%', 'A');
+					result = (b.data[statMade] / b.data[statAtt]) - (a.data[statMade] / a.data[statAtt]);
 				} else {
 					result = (b.data[statName] / b.data['GP']) - (a.data[statName] / a.data['GP']);
 				}
@@ -61,18 +71,33 @@ const StatLeaders = (props) => {
 				return result;
 			};
 
+			const filterCallback = (player) => {
+				if (statName.search(/%/) !== -1) {
+					return player.data[statName.replace('%', 'A')] > 20;
+				} else {
+					return true;
+				}
+			};
 
-			statLeaders[statName] = stats.sort(sortCallback).slice(0, 5).map(({name, data}, index) => {
+
+
+
+			statLeaders[statName] = stats.sort(sortCallback).filter(filterCallback).slice(0, 5).map(({name, data}, index) => {
 				let statValue;
 
 				if (statName === 'GP') {
-					statValue = data[statName];
+					statValue = Math.round(data[statName] * 10) / 10;
+				} else if (statName.search(/%/) !== -1) {
+					let statMade = statName.replace('%', 'M');
+					let statAtt = statName.replace('%', 'A');
+					let percentage = data[statMade] / data[statAtt];
+					statValue = `${Math.round(percentage * 1000) / 10}%`;
 				} else {
-					statValue = data[statName] / data['GP'];
+					statValue = Math.round((data[statName] / data['GP']) * 10) / 10;
 				}
 				return <tr key={index}>
 					<td>{name}</td>
-					<td>{Math.round(statValue * 10) / 10}</td>
+					<td>{statValue}</td>
 				</tr>
 			});
 		});
