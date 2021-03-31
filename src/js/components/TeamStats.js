cbfa1942025b6b89d772690c59015c9e6532ff88
mattfisher <nuggsrocks@yahoo.com>
2021-03-30 20:23:29 -0400

    added more stats

--- src/js/components/TeamStats.js
+++ src/js/components/TeamStats.js
@@ -5,12 +5,14 @@ export class TeamStats extends React.Component {
 	render() {
 		const {teamStats} = this.props;
 
-		let statKeys;
+		let statKeys, teams;
 
 		if (teamStats) {
 			statKeys = Object.keys(teamStats.seasonTotals.michigan);
+			teams = Object.keys(teamStats.seasonTotals);
 		}
 
+		console.log(teamStats);
 		return <section>
 			<h2>Team Stats</h2>
 			{
@@ -42,6 +44,38 @@ export class TeamStats extends React.Component {
 						</section>
 
 						<section>
+							<h3>Season Highs</h3>
+							<div className='flex'>
+
+								{
+									teams.map((team, index) => {
+										return <div key={index}>
+											<h4>{team[0].toUpperCase() + team.substring(1)} Season Highs</h4>
+											{
+												statKeys.map((key, index) => {
+													let text = teamStats.seasonHighsAndLows[team].seasonHighs[key];
+													return <h4 key={index}>{key}: {text}</h4>
+												})
+											}
+										</div>
+									})
+								}
+
+								{
+									teams.map((team, index) => {
+										return <div key={index}>
+											<h4>{team[0].toUpperCase() + team.substring(1)} Season Lows</h4>
+											{
+												statKeys.map((key, index) => {
+													let text = teamStats.seasonHighsAndLows[team].seasonLows[key];
+													return <h4 key={index}>{key}: {text}</h4>
+												})
+											}
+										</div>
+									})
+								}
+
+							</div>
 						</section>
 					</div>
 					:
